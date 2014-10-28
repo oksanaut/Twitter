f5f722ee625bfb88b579df78eed4bfa2691458dc
Oksana Timonin <one@oneapart.com>
2014-10-28 08:59:02 -0700

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Oksana Timonin on 28/10/2014.
+//  Copyright (c) 2014 oksanaut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Oksana Timonin on 28/10/2014.
+//  Copyright (c) 2014 oksanaut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
